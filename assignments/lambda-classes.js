8ac5a23310ca4017f9ccded634b1ac59549f34af
HitzeD <d-hitze@comcast.net>
2019-04-04 14:49:51 -0500

    created test objects

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -62,3 +62,86 @@ class ProjectManager extends Intructor{
         console.log(`${this.name} debugs ${obj.name}'s code on ${subject}`);
     }
 } // Project Manager instanceof Instructor
+
+// TEST OBJECTS
+
+const int1 = new Intructor({
+    name: "george",
+    age: 22,
+    gender: '?',
+    favLanguage: 'JavaScript',
+    specialty: 'cloud',
+    catchPhrase: 'Jumping Beans!'
+});
+
+const int2 = new Intructor({
+    name: "jim",
+    age: 44,
+    gender: 'F',
+    favLanguage: 'Swift',
+    specialty: 'things',
+    catchPhrase: 'Looking Beans!'
+});
+
+const int3 = new Intructor({
+    name: "tom",
+    age: 71,
+    gender: 'M',
+    favLanguage: 'Python',
+    specialty: 'stuff',
+    catchPhrase: 'Special Beans!'
+});
+
+const stu1 = new Student({
+    name: "exacto",
+    age: 4,
+    gender: '?',
+    favLanguage: 'babble',
+    specialty: 'not walking',
+    catchPhrase: 'Touch?'
+});
+
+const stu2 = new Student({
+    name: "sun",
+    age: 2,
+    gender: 'F',
+    favLanguage: 'c++',
+    specialty: 'engineer',
+    catchPhrase: 'climbing Beans!'
+});
+
+const stu3 = new Student({
+    name: "moon",
+    age: 1,
+    gender: '?',
+    favLanguage: 'Django',
+    specialty: 'software',
+    catchPhrase: 'computing Beans!'
+});
+
+const pm1 = new ProjectManager({
+    name: "artic",
+    age: 22,
+    gender: '?',
+    favLanguage: 'JavaScript',
+    specialty: 'teaching',
+    catchPhrase: 'learning Beans!'
+});
+
+const pm2 = new ProjectManager({
+    name: "oscar",
+    age: 32,
+    gender: '?',
+    favLanguage: 'objective c',
+    specialty: 'who knows',
+    catchPhrase: 'questioning Beans!'
+});
+
+const pm3 = new ProjectManager({
+    name: "link",
+    age: 72,
+    gender: 'M and only M',
+    favLanguage: 'Hyrule',
+    specialty: 'Water Temple',
+    catchPhrase: 'Do not hit those chickens!'
+});
