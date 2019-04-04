3968c7fc386d4a93ef3d487d4ab221000997363c
HitzeD <d-hitze@comcast.net>
2019-04-04 14:24:07 -0500

    finished base object

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
-// CODE here for your Lambda Classes
+class Person {
+    constructor(obj){
+        this.name = obj.name;
+        this.age = obj.age;
+        this.location = obj.location;
+    }
+    speak(){
+        console.log(`Hello, my name is ${this.name}. I am fomr ${this.location}!`)
+    }
+} // Person
+
